d77c6fb8a4e36cdc816d67f40c630cc7c7198ae6
openshift-pipelines-avi <konflux@no-reply.konflux-ci.dev>
2025-02-02 08:24:40 +0000

    OpenShift Pipelines Avi update echo-v02 Signed-off-by: openshift-pipelines-avi <konflux@no-reply.konflux-ci.dev>

--- hack/trusted-tasks-update.sh
+++ hack/trusted-tasks-update.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-# Resolves all tasks from the `/task` directory of this repository and any Task
-# bundles in the `$QUAY_NAMESPACES` quay.io namespaces. Runs `ec track bundle`
-# with those git and image references in order to catalogue the latest versions
-# in the trusted tasks list in `$INPUT_IMAGE` written to `$OUTPUT_IMAGE` (both
-# default to `quay.io/konflux-ci/tekton-catalog/data-acceptable-bundles:latest`).
+# Resolves a task from a given CONTEXT and from the IMAGE, both dirives from the pipelinerun
+# Create a data-acceptable-bundles oci image in the task's repository, contains refs to both bundle and git reference of the task 
 #
 # By default both git and image references are collected, what is collected can
 # be controlled by `$COLLECT` which can be set to `git` or `oci`
@@ -11,98 +8,50 @@
 # Parameters via environment variables:
 # COLLECT         - can be set to `oci` or `git` (defaults to both), what to
 #                   resolve
-# INPUT_IMAGE     - Conftest OCI bundle containing the trusted tasks list, defaults
-#                   to:
-#                   `quay.io/konflux-ci/tekton-catalog/data-acceptable-bundles:latest`
-# OUTPUT_IMAGE    - Conftest OCI bundle to write the trusted task list to, defaults
-#                   to `$INPUT_IMAGE`
-# QUAY_NAMESPACES - Quay namespaces to query for Task bundles (defaults to
-#                   `redhat-appstudio-tekton-catalog konflux-ci/tekton-catalog`)
+# GIT_URL         - GIT URL contains the source code of the tasks
+# CONTEXT         - A subdirectory contining a specific task  
+# IMAGE           - The OCI task bundle built by the pipeline
+# OUTPUT_IMAGE    - Conftest OCI bundle to write the trusted task list to
+
 
 set -o errexit
 set -o nounset
 set -o pipefail
 set -ex
+
 mapfile -td ' ' COLLECT < <(echo -n "${COLLECT:-git oci}")
-# mapfile -td ' ' QUAY_NAMESPACES < <(
-#     echo -n "${QUAY_NAMESPACES:-avi_test/user-ns2/release-catalog}"
-# )
 
-: "${QUAY_NAMESPACES:=}"
+# IMAGE="quay.io/avi_test/user-ns2/pull-request-task-bundles/task-echo-v02:on-pr-e0c103f2aa15b14b9feff1b5922d0b50659316b8"
+# GIT_URL="https://github.com/avi-biton/task-test"
+# CONTEXT="task/echo/0.2"
+OUTPUT_IMAGE=$(echo ${IMAGE} | awk -F: '{sub(/:.*/, "/data-acceptable-bundles:latest"); print}')
 
-# INPUT_IMAGE=${INPUT_IMAGE:-quay.io/avi_test/user-ns2/release-catalog/data-acceptable-bundles:latest}
-# OUTPUT_IMAGE=${OUTPUT_IMAGE:-$INPUT_IMAGE}
-# GIT_REPOSITORY=git+https://github.com/avi-biton/task-test
 
-function list_tasks() {
-    local full_namespace=$1
-    local toplevel_namespace=${full_namespace%%/*}
-
-    curl -sSL "https://quay.io/api/v1/repository?namespace=${toplevel_namespace}&public=true" -H 'Accept: application/json' |
-        jq --arg full_namespace "$full_namespace" -r '
-            .repositories[]
-            | "\(.namespace)/\(.name)"
-            | select(test("^\($full_namespace)/task-[^/]*$"))
-            | "quay.io/\(.)"
-        '
-}
-
-function list_tasks_in_all_namespaces() {
-    for namespace in "${QUAY_NAMESPACES[@]}"; do
-        tasks_list+=$(list_tasks "$namespace")
-    done
-}
-
-HACK_DIR="$(dirname "${BASH_SOURCE[0]}")"
-
-git_params=()
-oci_params=()
-tasks_list=()
+git_params=""
+oci_params=""
 for c in "${COLLECT[@]}"; do
   case "${c}" in
     git)
-      echo -n Resolving git Tasks
-      for task_dir in "${HACK_DIR}"/../task/*/*; do
-        [ ! -d "${task_dir}" ] && continue
-        [ -f "${task_dir}/kustomization.yaml" ] && continue
-        mapfile -td '/' dirparts < <(echo "${task_dir}")
-        task_file="${task_dir}/${dirparts[-2]}.yaml"
-        [ ! -f "${task_file}" ] && continue
-
-        task_git_url=${GIT_REPOSITORY}//${task_file/#*task/task}
-        git_params+=("--git=${task_git_url}")
-        echo -n .
-      done
+      echo -n Adding git Task reference
+      task_dir=${GIT_URL}//${CONTEXT}
+      mapfile -td '/' dirparts < <(echo "${task_dir}")
+      task_file="${task_dir}/${dirparts[-2]}.yaml"
+      git_params=("--git=git+${task_file}")
       echo
       echo Collected git parameters:
-      printf "%s\n" "${git_params[@]}"
+      printf "%s\n" "${git_params}"
       ;;
     oci)
-      echo -n Resolving OCI Tasks bundles
-      tasks_list=$(list_tasks ${QUAY_NAMESPACES[0]})
-      for repository in $tasks_list; do
-        mapfile -t refs < <(
-          skopeo list-tags docker://"${repository}" |
-            jq --arg repository "${repository}" -r '.Tags[] | select(test("^(\\d+)(\\.\\d)*$")) | "\($repository):\(.)"'
-        )
-        for ref in "${refs[@]}"; do
-          oci_params+=("--bundle=${ref}")
-        done
-        echo -n .
-      done
+      echo -n Adding OCI Task bundle
+      oci_params=("--bundle=${IMAGE}")
       echo
       echo Collected OCI parameters:
-      printf "%s\n" "${oci_params[@]}"
+      printf "%s\n" "${oci_params}"
   esac
 done
 
 echo Running:
 PS4=''
 set -x
-ec track bundle \
-  --freshen \
-  --replace \
-  --input "oci:${INPUT_IMAGE}" \
-  --output "oci:${OUTPUT_IMAGE}" \
-  "${git_params[@]}" \
-  "${oci_params[@]}"
+ec track bundle --freshen ${git_params} ${oci_params} --output oci:${OUTPUT_IMAGE}
+  
